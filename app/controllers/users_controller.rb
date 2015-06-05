17d1e489b507748dc21659c3e2f287ebe21803a2
Denis Kryukov <denis.kryukov@aol.com>
2015-06-05 16:01:55 +0300

    set @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,20 @@
 class UsersController < ApplicationController
   def index
-
   end
 
   def show
-
   end
 
   def update
-
   end
 
   def destroy
-
   end
 
   def finish_signup
     if request.patch? && params[:user] #&& params[:user][:email]
+      @user = User.find_by_email(user_params[:email])
       if @user.update(user_params)
-        @user.skip_reconfirmation!
         sign_in(@user, :bypass => true)
         redirect_to edit_user_registration_path , notice: 'Your profile was successfully updated.'
       else
