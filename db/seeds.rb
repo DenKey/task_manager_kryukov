0fde63e71361f484ea0f923a9b53320ec98032ad
Denis Kryukov <denis.kryukov@aol.com>
2015-06-09 14:13:13 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 User.create!(email: "admin@example.com", password: "zero21nmu", admin: true)
+
+
+(1..5).each do |num|
+  user = User.create!(email: "user#{num}@example.com", password: "12345678")
+  list = user.list.create(title: "User#{num} List ")
+  list.todo.create(content: "Just Do it")
+end
+
